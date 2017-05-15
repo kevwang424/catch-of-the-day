44fdb708f8e697d4e3445aa08a7747e0d608853c
Kevin Wang <kev.wang424@gmail.com>
2017-05-15 15:31:04 -0400

    added components

---
+++ src/components/StorePicker.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class StorePicker extends React.Component {
+
+  render(){
+    return (
+      <form className="store-selctor">
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name"></input>
+        <button type="submit">Visit Store -></button>
+      </form>
+    )
+  }
+}
+
+export default StorePicker
