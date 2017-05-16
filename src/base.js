26fbbcd1188b6f5dbabb6eeda1ebfb045f5e200e
Kevin Wang <kev.wang424@gmail.com>
2017-05-15 20:56:14 -0400

    added lifecycle hooks to App component. Added localStorage for order

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base'
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyDIPFCLJJT6ckYwhGYZXIGpZl2jfZi1_jg",
+  authDomain: "catch-of-the-day-kevin-w.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-kevin-w.firebaseio.com"
+})
+
+export default base
--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,8 @@ import Inventory from './Inventory'
 import Fish from './Fish'
 
 import sampleFishes from '../sample-fishes'
+import base from '../base'
+
 class App extends React.Component {
 
   constructor(){
@@ -17,6 +19,32 @@ class App extends React.Component {
       fishes: {},
       order: {}
     }
+  }
+
+  componentWillMount(){
+    //this runs right before the <App> is rendered
+    this.ref = base.syncState(`${this.props.params.storeId}/fishes` , {
+      context: this,
+      state: 'fishes'
+    })
+
+    //check if there is any order in localStorage
+    const localStorageRef = localStorage.getItem(`order-${this.props.params.storeId}`)
+
+    if(localStorageRef){
+      //update our App components order state
+      this.setState({
+        order: JSON.parse(localStorageRef)
+      })
+    }
+  }
+
+  componentWillUnmount(){
+    base.removeBinding(this.ref)
+  }
+
+  componentWillUpdate(nextProps, nextState){
+    localStorage.setItem(`order-${this.props.params.storeId}`, JSON.stringify(nextState.order))
   }
 
   addFish(fish){
@@ -56,7 +84,7 @@ class App extends React.Component {
             }
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order}/>
+        <Order fishes={this.state.fishes} order={this.state.order} params={this.props.params}/>
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     )
