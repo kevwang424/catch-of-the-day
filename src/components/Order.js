46da96888ba7c1f96f1e1a60417c6b8e016f78f2
Kevin Wang <kev.wang424@gmail.com>
2017-06-01 11:32:16 -0400

    added animation for adding to order and increasing count

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import CSSTransitionGroup from 'react-addons-css-transition-group'
 
 import { formatPrice } from '../helpers'
 
@@ -21,7 +22,18 @@ class Order extends React.Component{
 
     return (
       <li key={key}>
-        <span>{count}lbs {fish.name} {removeButton}</span>
+        <span>
+          <CSSTransitionGroup
+            component="span"
+            className="count"
+            transitionName="count"
+            transitionEnterTimeout={250}
+            transitionLeaveTimeout={250}
+            >
+            <span key={count}>{count}</span>
+          </CSSTransitionGroup>
+          lbs {fish.name} {removeButton}
+        </span>
         <span className="price">{formatPrice(count*fish.price)}</span>
       </li>
     )
@@ -44,13 +56,18 @@ class Order extends React.Component{
     return (
       <div className="order-wrap">
         <h2>Your Order</h2>
-        <ul className="order">
+        <CSSTransitionGroup
+          className="order"
+          component="ul"
+          transitionName="order"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}>
           {orderIds.map(this.renderOrder)}
           <li className="total">
             <strong>Total:</strong>
             {formatPrice(total)}
           </li>
-        </ul>
+        </CSSTransitionGroup>
       </div>
     )
   }
